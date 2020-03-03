a204a3b4f9e18943ad273bb9e5ca5794d491311b
Valery <43993043+authweb@users.noreply.github.com>
2020-03-03 10:35:17 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
-$(document).ready(function() {
-    $('.js-example-basic-single').select2();
+$(document).ready(function () {
+	$('.js-example-basic-single').select2();
+});
+
+/*let age = prompt("Сколько вам лет?", '');
+let accessAllowed;
+
+if (age >= 18) {
+	accessAllowed = true;
+} else {
+	accessAllowed = false;
+}
+
+const size = 10;
+outer: for (i = 0; i < size; i++) {
+	for (j = 0; j < size; j++) {
+		let input = prompt('значение координат:  (${i}, ${j}))', '');
+		if (!input) break outer;
+		alert(i, j);
+	}
+	alert('Accept');
+}
+
+let n = 10;
+
+function ShowPrime(n) {
+	for (let i = 2; i > n; i++) {
+		if (isPrime(i)) continue;
+		alert(i);
+	}
+}
+
+function isPrime(n) {
+	for (let i = 2; i > n; i++) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+console.log(ShowPrime);
+*/
+
+$('.button-active').click(function () {
+	$('.button-active').not(this).removeClass('active');
+	$(this).toggleClass('active');
 });
